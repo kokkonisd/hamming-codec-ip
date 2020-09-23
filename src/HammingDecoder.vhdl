b08d6a200afbbdddf457d94281c33812dce87f0e
Dimitris Kokkonis <kokkonisd@gmail.com>
2020-09-23 16:23:47 +0200

    Add comments to explain decoder

--- src/HammingDecoder.vhdl
+++ src/HammingDecoder.vhdl
@@ -23,20 +23,24 @@ end entity;
 
 
 architecture default of HammingDecoder is
-    signal group1: std_logic;
-    signal group2: std_logic;
-    signal group3: std_logic;
+    signal group1: std_logic; -- tied to data digits 0, 1 and 3
+    signal group2: std_logic; -- tied to data digits 0, 2 and 3
+    signal group3: std_logic; -- tied to data digits 1, 2 and 3
 begin
 
---  D4 D3 D2 P3 D1 P2 P1
-
+    -- The input vector has as follows (D are data bits, P are parity bits):
+    -- [MSB] D3 D2 D1 P2 D0 P1 P0 [LSB]
     group1 <= DataIn(0) xor DataIn(2) xor DataIn(4) xor DataIn(6);
     group2 <= DataIn(1) xor DataIn(2) xor DataIn(5) xor DataIn(6);
     group3 <= DataIn(3) xor DataIn(4) xor DataIn(5) xor DataIn(6);
 
+    -- If groups 1 and 2 are triggered, D0 must be flipped
     DataOut(0) <= (group1       and group2       and (not group3)) xor DataIn(2);
+    -- If groups 1 and 3 are triggered, D1 must be flipped
     DataOut(1) <= (group1       and (not group2) and group3)       xor DataIn(4);
+    -- If groups 2 and 3 are triggered, D2 must be flipped
     DataOut(2) <= ((not group1) and group2       and group3)       xor DataIn(5);
+    -- If all groups are triggered, D3 must be flipped
     DataOut(3) <= (group1       and group2       and group3)       xor DataIn(6);
 
 end architecture;
