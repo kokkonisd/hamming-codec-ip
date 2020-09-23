a8e92c39acbc10466f951cfabe906718060874d4
Dimitris Kokkonis <kokkonisd@gmail.com>
2020-09-23 16:19:43 +0200

    Test every noisy case for decoder

--- tb/HammingDecoder_tb.vhdl
+++ tb/HammingDecoder_tb.vhdl
@@ -28,153 +28,208 @@ begin
         wait for 1 ns;
         assert DataOut = "0000" report "DataOut is wrong for input '0000000'" severity error;
 
-        DataIn <= "0001000";
-        wait for 1 ns;
-        assert DataOut = "0000" report "DataOut is wrong for noisy input '0001000'" severity error;
+        -- Test resiliency by flipping every bit once
+        for i in 0 to 6 loop
+            DataIn(i) <= not DataIn(i);
+            wait for 1 ns;
+            assert DataOut = "0000" report "DataOut is wrong for noisy input" severity error;
+            DataIn(i) <= not DataIn(i);
+        end loop;
 
 
         DataIn <= "0000111";
         wait for 1 ns;
         assert DataOut = "0001" report "DataOut is wrong for input '0000111'" severity error;
 
-        DataIn <= "0000110";
-        wait for 1 ns;
-        assert DataOut = "0001" report "DataOut is wrong for noisy input '0000110'" severity error;
+        -- Test resiliency by flipping every bit once
+        for i in 0 to 6 loop
+            DataIn(i) <= not DataIn(i);
+            wait for 1 ns;
+            assert DataOut = "0001" report "DataOut is wrong for noisy input" severity error;
+            DataIn(i) <= not DataIn(i);
+        end loop;
 
 
         DataIn <= "0011001";
         wait for 1 ns;
         assert DataOut = "0010" report "DataOut is wrong for input '0011001'" severity error;
 
-        DataIn <= "0011011";
-        wait for 1 ns;
-        assert DataOut = "0010" report "DataOut is wrong for noisy input '0011011'" severity error;
-
-
-        DataIn <= "0011001";
-        wait for 1 ns;
-        assert DataOut = "0010" report "DataOut is wrong for input '0011001'" severity error;
-
-        DataIn <= "0001001";
-        wait for 1 ns;
-        assert DataOut = "0010" report "DataOut is wrong for noisy input '0001001'" severity error;
+        -- Test resiliency by flipping every bit once
+        for i in 0 to 6 loop
+            DataIn(i) <= not DataIn(i);
+            wait for 1 ns;
+            assert DataOut = "0010" report "DataOut is wrong for noisy input" severity error;
+            DataIn(i) <= not DataIn(i);
+        end loop;
 
 
         DataIn <= "0011110";
         wait for 1 ns;
         assert DataOut = "0011" report "DataOut is wrong for input '0011110'" severity error;
 
-        DataIn <= "0010110";
-        wait for 1 ns;
-        assert DataOut = "0011" report "DataOut is wrong for noisy input '0010110'" severity error;
+        -- Test resiliency by flipping every bit once
+        for i in 0 to 6 loop
+            DataIn(i) <= not DataIn(i);
+            wait for 1 ns;
+            assert DataOut = "0011" report "DataOut is wrong for noisy input" severity error;
+            DataIn(i) <= not DataIn(i);
+        end loop;
 
 
         DataIn <= "0101010";
         wait for 1 ns;
         assert DataOut = "0100" report "DataOut is wrong for input '0101010'" severity error;
 
-        DataIn <= "0100010";
-        wait for 1 ns;
-        assert DataOut = "0100" report "DataOut is wrong for noisy input '0100010'" severity error;
+        -- Test resiliency by flipping every bit once
+        for i in 0 to 6 loop
+            DataIn(i) <= not DataIn(i);
+            wait for 1 ns;
+            assert DataOut = "0100" report "DataOut is wrong for noisy input" severity error;
+            DataIn(i) <= not DataIn(i);
+        end loop;
 
 
         DataIn <= "0101101";
         wait for 1 ns;
         assert DataOut = "0101" report "DataOut is wrong for input '0101101'" severity error;
 
-        DataIn <= "1101101";
-        wait for 1 ns;
-        assert DataOut = "0101" report "DataOut is wrong for noisy input '1101101'" severity error;
+        -- Test resiliency by flipping every bit once
+        for i in 0 to 6 loop
+            DataIn(i) <= not DataIn(i);
+            wait for 1 ns;
+            assert DataOut = "0101" report "DataOut is wrong for noisy input" severity error;
+            DataIn(i) <= not DataIn(i);
+        end loop;
 
 
         DataIn <= "0110011";
         wait for 1 ns;
         assert DataOut = "0110" report "DataOut is wrong for input '0110011'" severity error;
 
-        DataIn <= "0110001";
-        wait for 1 ns;
-        assert DataOut = "0110" report "DataOut is wrong for noisy input '0110001'" severity error;
+        -- Test resiliency by flipping every bit once
+        for i in 0 to 6 loop
+            DataIn(i) <= not DataIn(i);
+            wait for 1 ns;
+            assert DataOut = "0110" report "DataOut is wrong for noisy input" severity error;
+            DataIn(i) <= not DataIn(i);
+        end loop;
 
 
         DataIn <= "0110100";
         wait for 1 ns;
         assert DataOut = "0111" report "DataOut is wrong for input '0110100'" severity error;
 
-        DataIn <= "0110101";
-        wait for 1 ns;
-        assert DataOut = "0111" report "DataOut is wrong for noisy input '0110101'" severity error;
+        -- Test resiliency by flipping every bit once
+        for i in 0 to 6 loop
+            DataIn(i) <= not DataIn(i);
+            wait for 1 ns;
+            assert DataOut = "0111" report "DataOut is wrong for noisy input" severity error;
+            DataIn(i) <= not DataIn(i);
+        end loop;
 
 
         DataIn <= "1001011";
         wait for 1 ns;
         assert DataOut = "1000" report "DataOut is wrong for input '1001011'" severity error;
 
-        DataIn <= "1011011";
-        wait for 1 ns;
-        assert DataOut = "1000" report "DataOut is wrong for noisy input '1011011'" severity error;
+        -- Test resiliency by flipping every bit once
+        for i in 0 to 6 loop
+            DataIn(i) <= not DataIn(i);
+            wait for 1 ns;
+            assert DataOut = "1000" report "DataOut is wrong for noisy input" severity error;
+            DataIn(i) <= not DataIn(i);
+        end loop;
 
 
         DataIn <= "1001100";
         wait for 1 ns;
         assert DataOut = "1001" report "DataOut is wrong for input '1001100'" severity error;
 
-        DataIn <= "1000100";
-        wait for 1 ns;
-        assert DataOut = "1001" report "DataOut is wrong for noisy input '1000100'" severity error;
+        -- Test resiliency by flipping every bit once
+        for i in 0 to 6 loop
+            DataIn(i) <= not DataIn(i);
+            wait for 1 ns;
+            assert DataOut = "1001" report "DataOut is wrong for noisy input" severity error;
+            DataIn(i) <= not DataIn(i);
+        end loop;
 
 
         DataIn <= "1010010";
         wait for 1 ns;
         assert DataOut = "1010" report "DataOut is wrong for input '1010010'" severity error;
 
-        DataIn <= "1010011";
-        wait for 1 ns;
-        assert DataOut = "1010" report "DataOut is wrong for noisy input '1010011'" severity error;
+        -- Test resiliency by flipping every bit once
+        for i in 0 to 6 loop
+            DataIn(i) <= not DataIn(i);
+            wait for 1 ns;
+            assert DataOut = "1010" report "DataOut is wrong for noisy input" severity error;
+            DataIn(i) <= not DataIn(i);
+        end loop;
 
 
         DataIn <= "1010101";
         wait for 1 ns;
         assert DataOut = "1011" report "DataOut is wrong for input '1010101'" severity error;
 
-        DataIn <= "0010101";
-        wait for 1 ns;
-        assert DataOut = "1011" report "DataOut is wrong for noisy input '0010101'" severity error;
+        -- Test resiliency by flipping every bit once
+        for i in 0 to 6 loop
+            DataIn(i) <= not DataIn(i);
+            wait for 1 ns;
+            assert DataOut = "1011" report "DataOut is wrong for noisy input" severity error;
+            DataIn(i) <= not DataIn(i);
+        end loop;
 
 
         DataIn <= "1100001";
         wait for 1 ns;
         assert DataOut = "1100" report "DataOut is wrong for input '1100001'" severity error;
 
-        DataIn <= "1100011";
-        wait for 1 ns;
-        assert DataOut = "1100" report "DataOut is wrong for noisy input '1100011'" severity error;
+        -- Test resiliency by flipping every bit once
+        for i in 0 to 6 loop
+            DataIn(i) <= not DataIn(i);
+            wait for 1 ns;
+            assert DataOut = "1100" report "DataOut is wrong for noisy input" severity error;
+            DataIn(i) <= not DataIn(i);
+        end loop;
 
 
         DataIn <= "1100110";
         wait for 1 ns;
         assert DataOut = "1101" report "DataOut is wrong for input '1100110'" severity error;
 
-        DataIn <= "1101110";
-        wait for 1 ns;
-        assert DataOut = "1101" report "DataOut is wrong for noisy input '1101110'" severity error;
+        -- Test resiliency by flipping every bit once
+        for i in 0 to 6 loop
+            DataIn(i) <= not DataIn(i);
+            wait for 1 ns;
+            assert DataOut = "1101" report "DataOut is wrong for noisy input" severity error;
+            DataIn(i) <= not DataIn(i);
+        end loop;
 
 
         DataIn <= "1111000";
         wait for 1 ns;
         assert DataOut = "1110" report "DataOut is wrong for input '1111000'" severity error;
 
-        DataIn <= "1101000";
-        wait for 1 ns;
-        assert DataOut = "1110" report "DataOut is wrong for noisy input '1101000'" severity error;
+        -- Test resiliency by flipping every bit once
+        for i in 0 to 6 loop
+            DataIn(i) <= not DataIn(i);
+            wait for 1 ns;
+            assert DataOut = "1110" report "DataOut is wrong for noisy input" severity error;
+            DataIn(i) <= not DataIn(i);
+        end loop;
 
 
         DataIn <= "1111111";
         wait for 1 ns;
         assert DataOut = "1111" report "DataOut is wrong for input '1111111'" severity error;
 
-        DataIn <= "1111011";
-        wait for 1 ns;
-        assert DataOut = "1111" report "DataOut is wrong for noisy input '1111011'" severity error;
+        -- Test resiliency by flipping every bit once
+        for i in 0 to 6 loop
+            DataIn(i) <= not DataIn(i);
+            wait for 1 ns;
+            assert DataOut = "1111" report "DataOut is wrong for noisy input" severity error;
+            DataIn(i) <= not DataIn(i);
+        end loop;
     end process;
 
 end architecture;
