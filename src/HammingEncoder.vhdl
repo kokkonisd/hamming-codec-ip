71095910832992db19c4df045be91e091fd8009e
Dimitris Kokkonis <kokkonisd@gmail.com>
2020-09-26 03:42:03 +0200

    Make encoder generic

--- src/HammingEncoder.vhdl
+++ src/HammingEncoder.vhdl
@@ -1,6 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.math_real.all;
 
 
 -- Hamming encoder
@@ -17,29 +18,79 @@ use ieee.numeric_std.all;
 
 
 entity HammingEncoder is
+    generic (
+        BlockSize : integer range 8 to 256
+    );
     port (
-        DataIn : in  std_logic_vector (3 downto 0);
-        DataOut: out std_logic_vector (6 downto 0)
+        DataIn  : in  std_logic_vector ((BlockSize - integer(ceil(log2(real(BlockSize)))) - 2) downto 0);
+        DataOut : out std_logic_vector ((BlockSize - 1) downto 0)
     );
 end entity;
 
 
 architecture default of HammingEncoder is
+    constant parity_bits : integer := integer(ceil(log2(real(BlockSize))));
+    signal encoded_data : std_logic_vector ((BlockSize - 1) downto 0);
 begin
 
-    -- The output vector has as follows (D are data bits, P are parity bits):
-    -- [MSB] P2 P1 P0 D3 D2 D1 D0 [LSB]
-    --
-    -- where: P0 = D0 xor D1 xor D3
-    --        P1 = D0 xor D2 xor D3
-    --        P2 = D1 xor D2 xor D3
+    encoding: process(DataIn)
+        -- Counter variable
+        variable j : integer := 0;
+        -- Variable to help index bits of counter
+        variable bit_index : std_logic_vector (7 downto 0);
+        -- Variable to help calculate powers of 2 via shifting
+        variable pow_2_index : std_logic_vector (7 downto 0);
+        -- Variable to help calculate parity
+        variable parity : std_logic;
+        -- Scratch vector to hold the result of all calculations
+        variable scratch_vector : std_logic_vector ((BlockSize - 1) downto 0);
+    begin
+        -- Initialize output vector to 0
+        scratch_vector := (others => '0');
 
-    DataOut(0) <= DataIn(0);
-    DataOut(1) <= DataIn(1);
-    DataOut(2) <= DataIn(2);
-    DataOut(3) <= DataIn(3);
-    DataOut(4) <= DataIn(0) xor DataIn(1) xor DataIn(3);
-    DataOut(5) <= DataIn(0) xor DataIn(2) xor DataIn(3);
-    DataOut(6) <= DataIn(1) xor DataIn(2) xor DataIn(3);
+        if (DataIn /= (DataIn'range => 'U')) then
+            -- Initialize output vector with input message values
+            j := 0;
+            for i in 3 to (BlockSize - 1) loop
+                if (integer(ceil(log2(real(i)))) /= integer(floor(log2(real(i))))) then
+                    scratch_vector(i) := DataIn(j);
+                    j := j + 1;
+                end if;
+            end loop;
+
+            -- Set parity bits accordingly 
+            for i in 0 to (parity_bits - 1) loop
+                -- Move to the next power of 2 in terms of index
+                pow_2_index := "00000000";
+                pow_2_index(i) := '1';
+                parity := '0';
+
+                -- Iterate over all bits of the block
+                for k in 1 to (BlockSize - 1) loop
+                    bit_index := std_logic_vector(to_unsigned(k, 8));
+                    -- If the i-th bit (i corresponding to the i-th parity bit) is 1, this element is in the i-th
+                    -- parity group, so we need to take it into account when calculating parity
+                    if (bit_index(i) = '1') then
+                        parity := parity xor scratch_vector(k);
+                    end if;
+                end loop;
+
+                -- Set the final parity for this parity group
+                scratch_vector(to_integer(unsigned(pow_2_index))) := parity;
+            end loop;
+
+            -- Set global parity bit (LSB)
+            parity := '0';
+            for i in 1 to (BlockSize - 1) loop
+                parity := parity xor scratch_vector(i);
+            end loop;
+            scratch_vector(0) := parity;
+        end if;
+
+        -- Update signal based on the final form of the scratch vector
+        encoded_data <= scratch_vector;
+    end process;
+
+    DataOut <= encoded_data;
 
 end architecture;
--- tb/HammingEncoder_tb.vhdl
+++ tb/HammingEncoder_tb.vhdl
@@ -15,78 +15,79 @@ end entity;
 
 
 architecture default of HammingEncoder_tb is
-    signal DataIn : std_logic_vector (3 downto 0);
-    signal DataOut: std_logic_vector (6 downto 0);
+    signal BlockSize : integer          := 8;
+    signal DataIn    : std_logic_vector (3 downto 0);
+    signal DataOut   : std_logic_vector (7 downto 0);
 
 begin
 
-    T0: entity work.HammingEncoder(default) port map (DataIn, DataOut);
+    T0: entity work.HammingEncoder(default) generic map (BlockSize) port map (DataIn, DataOut);
 
     test: process
     begin
         DataIn <= "0000";
         wait for 1 ns;
-        assert DataOut = "0000000" report "DataOut is wrong for input '0000'" severity error;
+        assert DataOut = "00000000" report "DataOut is wrong for input '0000'" severity error;
 
         DataIn <= "0001";
         wait for 1 ns;
-        assert DataOut = "0110001" report "DataOut is wrong for input '0001'" severity error;
+        assert DataOut = "00001111" report "DataOut is wrong for input '0001'" severity error;
 
         DataIn <= "0010";
         wait for 1 ns;
-        assert DataOut = "1010010" report "DataOut is wrong for input '0010'" severity error;
+        assert DataOut = "00110011" report "DataOut is wrong for input '0010'" severity error;
     
         DataIn <= "0011";
         wait for 1 ns;
-        assert DataOut = "1100011" report "DataOut is wrong for input '0011'" severity error;
+        assert DataOut = "00111100" report "DataOut is wrong for input '0011'" severity error;
     
         DataIn <= "0100";
         wait for 1 ns;
-        assert DataOut = "1100100" report "DataOut is wrong for input '0100'" severity error;
+        assert DataOut = "01010101" report "DataOut is wrong for input '0100'" severity error;
 
         DataIn <= "0101";
         wait for 1 ns;
-        assert DataOut = "1010101" report "DataOut is wrong for input '0101'" severity error;
+        assert DataOut = "01011010" report "DataOut is wrong for input '0101'" severity error;
 
         DataIn <= "0110";
         wait for 1 ns;
-        assert DataOut = "0110110" report "DataOut is wrong for input '0110'" severity error;
+        assert DataOut = "01100110" report "DataOut is wrong for input '0110'" severity error;
 
         DataIn <= "0111";
         wait for 1 ns;
-        assert DataOut = "0000111" report "DataOut is wrong for input '0111'" severity error;
+        assert DataOut = "01101001" report "DataOut is wrong for input '0111'" severity error;
 
         DataIn <= "1000";
         wait for 1 ns;
-        assert DataOut = "1111000" report "DataOut is wrong for input '1000'" severity error;
+        assert DataOut = "10010110" report "DataOut is wrong for input '1000'" severity error;
 
         DataIn <= "1001";
         wait for 1 ns;
-        assert DataOut = "1001001" report "DataOut is wrong for input '1001'" severity error;
+        assert DataOut = "10011001" report "DataOut is wrong for input '1001'" severity error;
 
         DataIn <= "1010";
         wait for 1 ns;
-        assert DataOut = "0101010" report "DataOut is wrong for input '1010'" severity error;
+        assert DataOut = "10100101" report "DataOut is wrong for input '1010'" severity error;
 
         DataIn <= "1011";
         wait for 1 ns;
-        assert DataOut = "0011011" report "DataOut is wrong for input '1011'" severity error;
+        assert DataOut = "10101010" report "DataOut is wrong for input '1011'" severity error;
 
         DataIn <= "1100";
         wait for 1 ns;
-        assert DataOut = "0011100" report "DataOut is wrong for input '1100'" severity error;
+        assert DataOut = "11000011" report "DataOut is wrong for input '1100'" severity error;
 
         DataIn <= "1101";
         wait for 1 ns;
-        assert DataOut = "0101101" report "DataOut is wrong for input '1101'" severity error;
+        assert DataOut = "11001100" report "DataOut is wrong for input '1101'" severity error;
 
         DataIn <= "1110";
         wait for 1 ns;
-        assert DataOut = "1001110" report "DataOut is wrong for input '1110'" severity error;
+        assert DataOut = "11110000" report "DataOut is wrong for input '1110'" severity error;
 
         DataIn <= "1111";
         wait for 1 ns;
-        assert DataOut = "1111111" report "DataOut is wrong for input '1111'" severity error;
+        assert DataOut = "11111111" report "DataOut is wrong for input '1111'" severity error;
     end process;
 
 end architecture;
